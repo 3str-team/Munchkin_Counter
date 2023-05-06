f544c695aed16f25cf6235beee02250824f2b6b3
3str-team <3strteam@gmailcom>
2023-05-06 10:09:06 +0300

    feat: save current points to local storage

--- src/App.js
+++ src/App.js
@@ -1,57 +1,58 @@
-import React, { useEffect, useState } from "react";
-import Counter from "./components/Counter/Counter";
-import Random from "./components/Random/Random";
-import Total from "./components/Total/Total";
+import React, { useEffect, useState } from 'react';
+import Counter from './components/Counter/Counter';
+import Random from './components/Random/Random';
+import Total from './components/Total/Total';
 
-import "./styles/app.css";
+import './styles/app.css';
+import { useStorageStateNumber } from './hooks/useStorageStateNumber';
 
 function App() {
-  const [level, setLevel] = useState(1);
-  const [items, setItems] = useState(0);
-  const [total, setTotal] = useState(level + items);
+	const [level, setLevel] = useStorageStateNumber(1, 'level');
+	const [items, setItems] = useStorageStateNumber(0, 'items');
+	const [total, setTotal] = useState(Number(level) + Number(items));
 
-  useEffect(() => {
-    setTotal(level + items);
-  }, [level, items]);
+	useEffect(() => {
+		setTotal(level + items);
+	}, [level, items]);
 
-  const incrementLevel = () => {
-    setLevel((level) => Math.min(10, level + 1));
-  };
+	const incrementLevel = () => {
+		setLevel(Math.min(10, level + 1));
+	};
 
-  const decrementLevel = () => {
-    setLevel((level) => Math.max(1, level - 1));
-  };
+	const decrementLevel = () => {
+		setLevel(Math.max(1, level - 1));
+	};
 
-  const incrementItems = () => {
-    setItems((items) => items + 1);
-  };
+	const incrementItems = () => {
+		setItems(items + 1);
+	};
 
-  const decrementItems = () => {
-    setItems((items) => items - 1);
-  };
+	const decrementItems = () => {
+		setItems(items - 1);
+	};
 
-  return (
-    <div className="App">
-      <Random />
-      <Total value={total} />
-      <div className="counters">
-        <Counter
-          title={"Уровень"}
-          value={level}
-          increment={incrementLevel}
-          decrement={decrementLevel}
-          reset={() => setLevel(1)}
-        />
-        <Counter
-          title={"Шмотки"}
-          value={items}
-          increment={incrementItems}
-          decrement={decrementItems}
-          reset={() => setItems(0)}
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Random />
+			<Total value={total} />
+			<div className="counters">
+				<Counter
+					title={'Уровень'}
+					value={level}
+					increment={incrementLevel}
+					decrement={decrementLevel}
+					reset={() => setLevel(1)}
+				/>
+				<Counter
+					title={'Шмотки'}
+					value={items}
+					increment={incrementItems}
+					decrement={decrementItems}
+					reset={() => setItems(0)}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
