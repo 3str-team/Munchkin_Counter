5c7c699222605a00c991b0feb34cfbe249795f77
3str-team <119620887+3str-webdev@users.noreply.github.com>
2023-10-02 15:54:58 +0300

    update: add client side denerating for random numers

--- src/components/Random/Random.jsx
+++ src/components/Random/Random.jsx
@@ -1,66 +1,38 @@
-import React from "react";
-import { useState, useRef } from "react";
-import axios from "axios";
+import React, { useState } from "react";
+import { random } from "../../lib/random";
 import ProgressBar from "../ProgressBar/ProgressBar";
 
 const duration = 2000; // 3 sec
 
 const Random = () => {
-  const [randomValue, setRandomValue] = useState(null);
-  const timeViewRef = useRef();
-  timeViewRef.current = 100;
+  const [randomValue, setRandomValue] = useState({ value: null });
 
-  const getRandomValue = async () => {
-    setRandomValue(-1);
-    const response = await axios.post(
-      "https://api.random.org/json-rpc/1/invoke",
-      {
-        headers: {
-          contentType: "application/json",
-        },
-        jsonrpc: "2.0",
-        method: "generateIntegers",
-        params: {
-          apiKey: "e7149772-93ae-48bd-b9a4-ede77705d8f8",
-          n: 1,
-          min: 1,
-          max: 6,
-          replacement: true,
-        },
-        id: Date.now(),
-      }
-    );
+  const getRandomValue = () => {
+    setRandomValue({ value: random(1, 6), key: Date.now() });
+  };
 
-    setRandomValue(response.data.result.random.data[0]);
+  const resetRandomValue = () => {
+    setRandomValue((prev) => ({ ...prev, value: null }));
   };
 
   return (
     <section className="randomWrapper">
-      {/* <div className="elements"> */}
       <button className="randomBtn" onClick={getRandomValue}>
         <i className="bx bxs-dice-5"></i>
       </button>
-      {randomValue ? (
+      {randomValue.value && (
         <div className="value">
-          {randomValue !== -1 ? (
-            <div className="wrapper">
-              {randomValue}
-              <ProgressBar
-                duration={duration}
-                delay={10}
-                onFinish={() => {
-                  setRandomValue(null);
-                }}
-              />
-            </div>
-          ) : (
-            <div className="loader"></div>
-          )}
+          <div className="wrapper">
+            {randomValue.value}
+            <ProgressBar
+              key={randomValue.key}
+              duration={duration}
+              delay={10}
+              onFinish={resetRandomValue}
+            />
+          </div>
         </div>
-      ) : (
-        ""
       )}
-      {/* </div> */}
     </section>
   );
 };
